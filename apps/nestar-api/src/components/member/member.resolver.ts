8fb771c229c71a3a201b4af9fd75834105b320e5
Umar_Aliy <umaraliy092@gmail.com>
2024-08-27 17:03:57 +0900

    feat: developed getAgents GraphQL API

--- apps/nestar-api/src/components/member/member.resolver.ts
+++ apps/nestar-api/src/components/member/member.resolver.ts
@@ -1,7 +1,7 @@
 import { Mutation, Resolver, Query, Args } from "@nestjs/graphql";
 import { MemberService } from "./member.service";
-import { LoginInput, MemberInput } from "../../libs/dto/member/member.input";
-import { Member } from "../../libs/dto/member/member";
+import { AgentsInquiry, LoginInput, MemberInput } from "../../libs/dto/member/member.input";
+import { Member, Members } from "../../libs/dto/member/member";
 import { UseGuards } from "@nestjs/common";
 import { AuthGuard } from "../auth/guards/auth.guard";
 import { AuthMember } from "../auth/decorators/authMember.decorator";
@@ -37,6 +37,13 @@ export class MemberResolver {
 		console.log("Mutation: updateMember");
 		delete input._id;
 		return this.memberService.updateMember(memberId, input);
+	}
+
+	@UseGuards(WithoutGuard)
+	@Query(() => Members)
+	public async getAgents(@Args("input") input: AgentsInquiry, @AuthMember("id") memberId: ObjectId): Promise<Members> {
+		console.log("Query, getAgents");
+		return this.memberService.getAgents(memberId, input);
 	}
 
 	@UseGuards(AuthGuard)
