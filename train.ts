f6f0f04d5079a34c29eea27f84c77c392c81462c
Umar_Aliy <umaraliy092@gmail.com>
2024-09-05 20:39:15 +0900

    integrated comment module related DTO

--- train.ts
+++ train.ts
@@ -3,13 +3,11 @@
 // Shunday function yozing, u parametridagi string ichidagi raqam va sonlarni sonini sanasin.
 // MASALAN: countNumberAndLetters(“string152%\¥”) return {number:3, letter:6}
 
-const alphabet = "abcdefghijklmnopqrstuvwxyz";
-
 function countNumbersAndLetters(str: string): { string: number } {
 	const result = { number: 0, string: 0 };
 	for (let value of str) {
 		if (Number(value) >= 0) result.number++;
-		else if (alphabet.includes(value)) result.string++;
+		else if (value >= "a") result.string++;
 	}
 	return result;
 }
