a57c3d62d0d34d7bb8781f0f2c69ebcc376d4d7f
Umar_Aliy <umaraliy092@gmail.com>
2024-09-07 19:18:58 +0900

    feat: developed likeTargetMember GraphQL API

--- apps/nestar-api/src/components/member/member.resolver.ts
+++ apps/nestar-api/src/components/member/member.resolver.ts
@@ -94,6 +94,17 @@ export class MemberResolver {
 		return `Hi ${authMember.memberNick}, you are ${authMember.memberType}, memberId: ${authMember._id}`;
 	}
 
+	@UseGuards(AuthGuard)
+	@Mutation(() => Member)
+	public async likeTargetMember(
+		@Args("memberId") input: string,
+		@AuthMember("_id") memberId: ObjectId,
+	): Promise<Member> {
+		console.log("Mutation: likeTargetMember");
+		const likeRefId = shapeIntoMongoObjectId(input);
+		return await this.memberService.likeTargetMember(memberId, likeRefId);
+	}
+
 	//** ADMIN **//
 
 	@Roles(MemberType.ADMIN)
