65e1b8fe32cea5b8c0ce9dc856bf0d1e04a6262e
Umar_Aliy <umaraliy092@gmail.com>
2024-09-10 08:02:02 +0900

    fix: modified like interaction related logic

--- apps/nestar-api/src/components/board-article/board-article.service.ts
+++ apps/nestar-api/src/components/board-article/board-article.service.ts
@@ -14,7 +14,7 @@ import { ViewService } from "../view/view.service";
 import { BoardArticleStatus } from "../../libs/enums/board-article.enum";
 import { StatisticModifier, T } from "../../libs/types/common";
 import { BoardArticleUpdate } from "../../libs/dto/board-article/board-article.update";
-import { lookupMember, shapeIntoMongoObjectId } from "../../libs/config";
+import { lookupAuthMemberLiked, lookupMember, shapeIntoMongoObjectId } from "../../libs/config";
 import { LikeService } from "../like/like.service";
 import { LikeGroup } from "../../libs/enums/like.enum";
 import { LikeInput } from "../../libs/dto/like/like.input";
@@ -121,7 +121,7 @@ export class BoardArticleService {
 						list: [
 							{ $skip: (input.page - 1) * input.limit },
 							{ $limit: input.limit },
-							// meLiked
+							lookupAuthMemberLiked(memberId),
 							lookupMember,
 							{ $unwind: "$memberData" }
 						],
