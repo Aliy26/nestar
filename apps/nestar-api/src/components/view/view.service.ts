c5bda04eecf289303d44d99bc941d76278516ead
Umar_Aliy <umaraliy092@gmail.com>
2024-09-12 15:17:19 +0900

    feat: developed getVisited GraphQL API

--- apps/nestar-api/src/components/view/view.service.ts
+++ apps/nestar-api/src/components/view/view.service.ts
@@ -1,26 +1,69 @@
 import { Injectable } from "@nestjs/common";
 import { InjectModel } from "@nestjs/mongoose";
-import { Model } from "mongoose";
+import { Model, ObjectId } from "mongoose";
 import { View } from "../../libs/dto/view/view";
 import { ViewInput } from "../../libs/dto/view/view.input";
 import { T } from "../../libs/types/common";
+import { OrdinaryInquiry } from "../../libs/dto/property/property.input";
+import { Properties } from "../../libs/dto/property/property";
+import { lookupVisit } from "../../libs/config";
+import { ViewGroup } from "../../libs/enums/view.enum";
 
 @Injectable()
 export class ViewService {
-	constructor(@InjectModel("View") private readonly viewModel: Model<View>) {}
-	public async recordView(input: ViewInput): Promise<View | null> {
-		const viewExist = await this.checkViewExistence(input);
-		if (!viewExist) {
-			console.log("-- New View Insertion --");
-			return await this.viewModel.create(input);
-		} else return null;
-	}
+  constructor(@InjectModel("View") private readonly viewModel: Model<View>) {}
+  public async recordView(input: ViewInput): Promise<View | null> {
+    const viewExist = await this.checkViewExistence(input);
+    if (!viewExist) {
+      console.log("-- New View Insertion --");
+      return await this.viewModel.create(input);
+    } else return null;
+  }
 
-	private async checkViewExistence(input: ViewInput): Promise<View | null> {
-		const { memberId, viewRefId } = input;
-		const search: T = { memberId: memberId, viewRefId: viewRefId };
-		const result = await this.viewModel.findOne(search).exec();
-		console.log(result);
-		return result;
-	}
+  private async checkViewExistence(input: ViewInput): Promise<View | null> {
+    const { memberId, viewRefId } = input;
+    const search: T = { memberId: memberId, viewRefId: viewRefId };
+    const result = await this.viewModel.findOne(search).exec();
+    console.log(result);
+    return result;
+  }
+
+  public async getVisitedProperties(
+    memberId: ObjectId,
+    input: OrdinaryInquiry,
+  ): Promise<Properties> {
+    const { page, limit } = input;
+    const match: T = { viewGroup: ViewGroup.PROPERTY, memberId: memberId };
+
+    const data: T = await this.viewModel
+      .aggregate([
+        { $match: match },
+        { $sort: { updatedAt: -1 } },
+        {
+          $lookup: {
+            from: "properties",
+            localField: "viewRefId",
+            foreignField: "_id",
+            as: "visitedProperty",
+          },
+        },
+        { $unwind: "$visitedProperty" },
+        {
+          $facet: {
+            list: [
+              { $skip: (page - 1) * limit },
+              { $limit: limit },
+              lookupVisit,
+              { $unwind: "$visitedProperty.memberData" },
+            ],
+            metaCounter: [{ $count: "total" }],
+          },
+        },
+      ])
+      .exec();
+    const result: Properties = { list: [], metaCounter: data[0].metaCounter };
+    result.list = data[0].list.map((ele: T) => ele.visitedProperty);
+
+    return result;
+  }
 }
